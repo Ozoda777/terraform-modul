13c4cdbfaa8d026076775847453070fef37fd9e7
Ozoda <ozoda.87@icloud.com>
2022-05-17 19:06:36 -0400

    I fixset varible

--- CHILD/EC2/variable.tf
+++ CHILD/EC2/variable.tf
@@ -2,7 +2,7 @@ variable  "ami_id" {
     default = "ami-0c02fb55956c7d316"
 }
 
-variable "instan_type" {
+variable "instance_type" {
     default = "t2.micro"
 }
 
@@ -13,16 +13,4 @@ variable "instan_type" {
 variable "server_tag" {
     default = "app-server"
 }
-variable "ami_id" {
-    default = "ami-0c02fb55956c7d316"
-}
 
-variable "nstance_type" {
-    default = "t2.micro"   
-}
-# variable "subnet_id" {
-# }
-
-variable "server_tag" {
-    default = "app-server"
-}
